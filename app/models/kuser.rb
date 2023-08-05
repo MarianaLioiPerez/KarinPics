a62424ff4833f9364b88e6519e56412c65a43221
MarianaLioiPerez <lioiperezm@gmail.com>
2023-08-04 23:24:57 -0400

    proyecto final-finalisimo

--- app/models/kuser.rb
+++ app/models/kuser.rb
@@ -3,4 +3,8 @@ class Kuser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         has_one_attached :avatar
+         has_many :comments
+         has_many :photos, dependent: :destroy
 end
